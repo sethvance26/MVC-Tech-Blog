16659aeec44ce7524dc8fbb46f700f3d49785f9d
Seth Vance <sethvance26@gmail.com>
2021-05-24 13:57:12 -0400

    updating Post routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,35 +1,55 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newPost = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updatePost = await Post.update(
+      {
+        ...req.body,
+        user_id: req.session.user_id,
+      },
+
+      {
+          where: {
+            id: req.params.id
+      }
+});
+    res.status(200).json(updatePost)
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+      
+
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const postData = await Post.destroy({
       where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
+        id: req.params.id
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
