4dc3adb9686d5797d208266feff8adc625864d16
Seth Vance <sethvance26@gmail.com>
2021-05-24 12:58:25 -0400

    Adding comment model, updating index for models

--- models/Comment.js
+++ models/Comment.js
@@ -5,11 +5,46 @@ class Comment extends Model {}
 
 Comment.init (
     {
-
-    }
-)
-
-
-
-
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        title: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: 'user',
+              key: 'id'
+          },
+        },
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date_created: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: DataTypes.NOW,
+        },
+        post_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'post',
+            key: 'id',
+          },
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+      }
+    );
 module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -7,6 +7,20 @@ User.hasMany(Post, {
   onDelete: 'CASCADE'
 });
 
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+})
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+})
 
 
 module.exports = { User, Post, Comment };
