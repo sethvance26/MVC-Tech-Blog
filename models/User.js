05b73b3e61585e8407520a9c6563c504f37617fa
Seth Vance <sethvance26@gmail.com>
2021-05-24 12:25:34 -0400

    adding new model,  & updating model index.js

--- models/User.js
+++ models/User.js
@@ -19,14 +19,7 @@ User.init(
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
       unique: true,
-      validate: {
-        isEmail: true,
-      },
     },
     password: {
       type: DataTypes.STRING,
