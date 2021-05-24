8b9663351bca99e3b6380a35a93e335df9a9c653
Seth Vance <sethvance26@gmail.com>
2021-05-24 14:43:49 -0400

    starting to write profileRoutes

---
+++ controllers/profileRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { User, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
