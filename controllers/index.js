034f2f7788cde3b7f8c025189cb3cf1fbfe8d31c
Seth Vance <sethvance26@gmail.com>
2021-05-24 14:19:57 -0400

    adding routes to index file

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const profileRoutes = require('./profileRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes);
 
 module.exports = router;
