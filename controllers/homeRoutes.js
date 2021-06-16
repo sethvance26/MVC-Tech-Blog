879d6181b0eeaac84b8c28efdee4fd6c37518ee3
Seth <sethvance26@gmail.com>
2021-06-16 01:44:44 -0400

    Editing homeroute to a login page instead

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
     // Serialize data so the template can read it
     // const posts = postData.map((post) => post.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
+    // Pass serialized data and session flag into templat
     res.render('login', { 
       // posts, 
       // logged_in: req.session.logged_in 
